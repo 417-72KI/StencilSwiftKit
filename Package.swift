0a946b863219b4d8b9d7cf9a8dca7467bba4b8f3
417-72KI <417.72ki@gmail.com>
2019-10-27 00:42:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
@@ -23,5 +23,5 @@ let package = Package(
       ]
     )
   ],
-  swiftLanguageVersions: [4]
+  swiftLanguageVersions: [.v5]
 )
